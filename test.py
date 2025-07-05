d1f29719a73583808fceeb09621fbcdfd7e6a607
Thiên Phúc <20127681@student.hcmus.edu.vn>
2025-07-05 08:49:13 +0700

    add Urban100 and evalutation

--- test.py
+++ test.py
@@ -4,12 +4,16 @@ import cv2
 import numpy as np
 import torch
 import RRDBNet_arch as arch
+from skimage.metrics import structural_similarity as ssim
 
 model_path = 'models/RRDB_ESRGAN_x4.pth'  # models/RRDB_ESRGAN_x4.pth OR models/RRDB_PSNR_x4.pth
-device = torch.device('cuda')  # if you want to run on CPU, change 'cuda' -> cpu
-# device = torch.device('cpu')
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 test_img_folder = 'LR/*'
+hr_img_folder = 'HR/*'
+
+psnr_list = []
+ssim_list = []
 
 model = arch.RRDBNet(3, 3, 64, 23, gc=32)
 model.load_state_dict(torch.load(model_path), strict=True)
@@ -33,5 +37,26 @@ for path in glob.glob(test_img_folder):
     with torch.no_grad():
         output = model(img_LR).data.squeeze().float().cpu().clamp_(0, 1).numpy()
     output = np.transpose(output[[2, 1, 0], :, :], (1, 2, 0))
-    output = (output * 255.0).round()
+    output = (output * 255.0).round().astype(np.uint8)
     cv2.imwrite('results/{:s}_rlt.png'.format(base), output)
+    
+    hr_path = osp.join('HR', base.replace('_LR', '_HR') + '.png')
+    original = cv2.imread(hr_path, cv2.IMREAD_COLOR)
+    
+    # if original.shape != output.shape:
+    #     print(f'Skipping {base} due to shape mismatch')
+    #     continue
+    
+    psnr_value = cv2.PSNR(original, output)
+    psnr_list.append(psnr_value)
+    print(f"PSNR của {base}: {psnr_value:.2f} dB")
+    
+    ssim_value = ssim(original, output, multichannel=True, channel_axis=2)
+    ssim_list.append(ssim_value)
+    print(f"SSIM của {base}: {ssim_value:.2f} dB")
+    
+# Tính giá trị trung bình
+avg_psnr = np.mean(psnr_list)
+avg_ssim = np.mean(ssim_list)
+print(f"\nAverage PSNR: {avg_psnr:.2f} dB")
+print(f"Average SSIM: {avg_ssim:.4f}")
